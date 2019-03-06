6da8569a6bf5ba2b771a8a070146cc2d7fa91d8d
dmazzoni <davidemazzoni@yahoo.it>
2019-03-06 16:27:29 +0100

    Use Warnings-NG plugin instead of Android Lint plugin

--- vars/androidSmokeTest.groovy
+++ vars/androidSmokeTest.groovy
@@ -5,6 +5,7 @@ def call(Map config) {
     def moduleName = config.moduleName
     def credentialsId = config.credentialsId
     def gitBranch = config.gitBranch ?: 'master'
+    def lintReportPattern = config.lintReportPattern ?: '**/lint-results.xml'
 
     if (gitUri == null || moduleName == null || credentialsId == null) {
         throw new IllegalStateException('Missing configuration arguments')
@@ -20,7 +21,7 @@ def call(Map config) {
 
   	stage ('Analyze') {
   	    sh "./gradlew :${moduleName}:lint"
-  	    androidLint()
+  	    recordIssues tool: androidLintParser(pattern: lintReportPattern)
   	}
 
   	stage ('Build') {
