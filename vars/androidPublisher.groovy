26413b035ceccc4e9a38b47f83451d2370d2da53
Francesco Bianco <f.bianco@aliaslab.net>
2020-02-18 14:52:44 +0100

    Added HTML publisher

---
+++ vars/androidPublisher.groovy
@@ -0,0 +1,24 @@
+def publishHTML(String projectName, Bool isUnitTestOnly) {
+
+  pipeline {
+
+    def unitTestBaseDirectory = projectName + '/build/reports/tests/'
+    def instrumentedBaseDirectory = projectName + '/build/reports/androidTests/connected',
+    def releaseUnitTest = unitTestBaseDirectory + 'testReleaseUnitTest'
+    def debugUnitTest = unitTestBaseDirectory + 'testDebugUnitTest'
+
+    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true,
+    reportDir: debugUnitTest,
+    reportFiles: 'index.html', reportName: 'Debug Report', reportTitles: ''])
+    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true,
+    reportDir: releaseUnitTest,
+    reportFiles: 'index.html', reportName: 'Release Report', reportTitles: ''])
+
+    if !isUnitTestOnly {
+      publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true,
+      reportDir: instrumentedBaseDirectory,
+      reportFiles: 'index.html', reportName: 'Instrumented Report', reportTitles: ''])
+    }
+  }
+
+}
