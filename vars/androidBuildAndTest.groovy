ae7ffaaec6a926397c4f9b1ee2fcf50a620281fd
Enrico Bonaldo <asclepix@yahoo.it>
2020-10-01 09:23:19 +0200

    Test on Android 9 instead of 7

--- vars/androidBuildAndTest.groovy
+++ vars/androidBuildAndTest.groovy
@@ -16,7 +16,7 @@ def call(Map config) {
       def instrumentedTestCommand = "./gradlew :${moduleName}:createDebugCoverageReport"
         if (useAvd) {
             withEnv(['JAVA_OPTS=-XX:+IgnoreUnrecognizedVMOptions']) {
-                withAvd(hardwareProfile: 'pixel', systemImage: 'system-images;android-24;default;x86_64', abi: 'x86_64') {
+                withAvd(hardwareProfile: 'pixel', systemImage: 'system-images;android-28;default;x86_64', abi: 'x86_64') {
                     sh instrumentedTestCommand
                 }
             }
